e9bb91bb4f97483ec11a3fca919146b384b41c07
Shreya Sridhar <shreyamohak@outlook.com>
2020-10-29 03:38:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -9,7 +9,11 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
+def engage(date)
   puts state_log(date)
   date = generate_star_date
+  binding.pry
+  puts 'hello'
 end
+
+engage(11-11-1994)
